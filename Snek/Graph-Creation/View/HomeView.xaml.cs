84fcb3df4b8de47214df8daed7c85fd20b2e1b94
Dzovani Koller <dzovani.koller@outlook.de>
2026-03-15 13:40:12 +0100

    style: redesign the Snek Studio workspace

--- Snek/Graph-Creation/View/HomeView.xaml.cs
+++ Snek/Graph-Creation/View/HomeView.xaml.cs
@@ -1,6 +1,4 @@
-using Microsoft.Win32;
-using Snek.Core.Graphs;
-using System.IO;
+using Snek.Presentation;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -8,37 +6,24 @@ namespace Snek.Graph_Creation;
 
 public partial class HomeView : UserControl
 {
-    private readonly GraphDocumentSerializer _serializer = App.GetRequiredService<GraphDocumentSerializer>();
-
     public HomeView() => InitializeComponent();
 
-    private void Button_Click(object sender, RoutedEventArgs e) => Window.GetWindow(this)?.Close();
-
-    private void Button_Click_1(object sender, RoutedEventArgs e)
+    private void CreateGraphButton_Click(object sender, RoutedEventArgs e)
     {
-        new Create_Graph_Type().Show();
-        Window.GetWindow(this)?.Close();
-    }
-
-    private void Button_Click_2(object sender, RoutedEventArgs e)
-    {
-        var dialog = new OpenFileDialog { Filter = "Snek-Dateien (*.snek)|*.snek" };
-        if (dialog.ShowDialog() != true)
+        if (Window.GetWindow(this) is { } window)
         {
-            return;
-        }
-
-        try
-        {
-            var document = _serializer.Deserialize(File.ReadAllText(dialog.FileName));
-            new Graph_Creator(document).Show();
-            Window.GetWindow(this)?.Close();
-        }
-        catch (Exception exception) when (exception is IOException or UnauthorizedAccessException or FormatException)
-        {
-            MessageBox.Show(exception.Message, "Datei konnte nicht geöffnet werden", MessageBoxButton.OK, MessageBoxImage.Warning);
+            GraphNavigation.CreateGraph(window);
         }
     }
 
-    private void Button_Click_3(object sender, RoutedEventArgs e) => new Window1().Show();
+    private void OpenGraphButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (Window.GetWindow(this) is { } window)
+        {
+            GraphNavigation.OpenGraph(window);
+        }
+    }
+
+    private void SnakeButton_Click(object sender, RoutedEventArgs e) =>
+        new Window1 { Owner = Window.GetWindow(this) }.Show();
 }
